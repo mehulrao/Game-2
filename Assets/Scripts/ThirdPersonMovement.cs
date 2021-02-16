bb58b62d32062ab89819fc07c3e615b8bc4fa078
Mehul Rao <mehulrao@gmail.com>
2021-02-15 20:04:15 -0500

    better person

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -8,22 +8,43 @@ public class ThirdPersonMovement : MonoBehaviour
     public Transform cam;
 
     public float speed = 6f;
-
+    public float gravity = -9.8f;
     public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    private float turnSmoothVelocity;
 
+    private Vector3 vSpeed;
+    public float jumpSpeed = 6f;
+    public float jumpHeight = 3f;
+    Vector3 moveDir;
+
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     void Update() {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(vertical, 0f, -horizontal).normalized;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if(direction.magnitude >= 0.1) {
-            float targetAngle = Mathf.Atan2(-direction.x, -direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Euler(0f, targetAngle - 90, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.right;
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+        if(controller.isGrounded && vSpeed.y < 0){
+            vSpeed.y = -2f;
+        }
+        if(controller.isGrounded){
+            if(Input.GetButtonDown("Jump")) {
+                    vSpeed.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+        Debug.Log(controller.isGrounded);
+        vSpeed.y += gravity * Time.deltaTime;
+        controller.Move(vSpeed * Time.deltaTime);
     }
+
 }
